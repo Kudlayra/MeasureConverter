746bc05e9bad55f12fea6d4f2a8951e357292a69
Ruslan Kudlay <Kudlayra@gmail.com>
2022-05-12 02:02:03 +0500

    before create new branch

--- app/src/main/java/com/example/android/measureconverter/presentation/ui/main/MainViewModel.kt
+++ app/src/main/java/com/example/android/measureconverter/presentation/ui/main/MainViewModel.kt
@@ -7,13 +7,14 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.example.android.measureconverter.data.LengthUnit
 import com.example.android.measureconverter.data.ListOfUnits
+import com.example.android.measureconverter.data.source.local.UnitsDao
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import java.util.concurrent.Flow
 
-class MainViewModel(private val listOfUnits: ListOfUnits) : ViewModel() {
+class MainViewModel(private val listOfUnits: UnitsDao) : ViewModel() {
 
-    val list = listOfUnits.list()
+    val list = listOfUnits.getAll("length")
 
     private var _leftChosenUnit = MutableLiveData<String>()
     val leftChosenUnit: LiveData<String> = _leftChosenUnit
--- app/src/main/java/com/example/android/measureconverter/presentation/ui/main/MainViewModelFactory.kt
+++ app/src/main/java/com/example/android/measureconverter/presentation/ui/main/MainViewModelFactory.kt
@@ -3,13 +3,12 @@ package com.example.android.measureconverter.presentation.ui.main
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.example.android.measureconverter.data.ListOfUnits
+import com.example.android.measureconverter.data.source.local.UnitsDao
 
-class MainViewModelFactory: ViewModelProvider.Factory {
-
-    private val listOfUnits by lazy(LazyThreadSafetyMode.NONE) { ListOfUnits() }
+class MainViewModelFactory(private val unitsDao: UnitsDao): ViewModelProvider.Factory {
 
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
-        return MainViewModel(listOfUnits = listOfUnits) as T
+        return MainViewModel(listOfUnits = unitsDao) as T
     }
 
 }
